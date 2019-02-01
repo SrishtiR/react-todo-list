8f457a9d8a166ccf2bf42c46a6cfa5828088737c
srishtirawal <srishti.rawal2012@vit.ac.in>
2019-01-31 21:31:24 -0700

    Adding to git

---
+++ src/components/todoItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './todoItem.css';
+
+export default class TodoItem extends React.Component {
+	removeTodo(id) {
+		this.props.removeTodo(id);
+	}
+
+	render() {
+		return(
+			<div className="todoWrapper">
+				<button className="removeTodo"
+								size="sm" onClick={(e) => this.removeTodo(this.props.id)}>&times;</button>{this.props.todo.text}
+			</div>
+			);
+	}
+}
